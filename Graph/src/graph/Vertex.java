976afe069879ea16298b6ef29704a78816a21a69
b1zee <b1zee@gmail.com>
2018-02-18 18:49:30 +0000

    initial commit

---
+++ Graph/src/graph/Vertex.java
@@ -0,0 +1,48 @@
+
+import java.util.Objects;
+import java.util.Set;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Vertex {
+    private Data data;
+    private int degree;
+    
+    public Vertex(Data data){
+        this.data = data;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Vertex other = (Vertex) obj;
+        if (!Objects.equals(this.data, other.data)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
