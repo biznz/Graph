7600cd36cc547485b164c03f7f60f8a388c08101
b1zee Fox <b1zeefox@gmail.com>
2018-02-19 16:13:04 +0000

    added new classes

--- Graph/src/graph/Move.java
+++ Graph/src/graph/Move.java
@@ -1,23 +1,14 @@
-package graph;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package graph;
 
 /**
  *
- * @author user
+ * @author b1z
  */
-public class Edge {
-    private Vertex end1;
-    private Vertex end2;
-
-    public Edge(Vertex V1, Vertex V2){
-        this.end1 = V1;
-        this.end2 = V2;
-    }
-    
+public class Move {
     
 }
--- Graph/src/graph/Node.java
+++ Graph/src/graph/Node.java
@@ -1,3 +1,4 @@
+package graph;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +9,21 @@
  *
  * @author user
  */
-public class Node {
+public class Node implements Abstract_Node{
+    
+    private Node parent;
+    private State state;
+    private Move move;
+    private int depth;
+    private int cost;
+
+    public Node(Node parent, State state, Move move, int depth, int cost) {
+        this.parent = parent;
+        this.state = state;
+        this.move = move;
+        this.depth = depth;
+        this.cost = cost;
+    }
+    
     
 }
